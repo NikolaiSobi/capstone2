8ded070bf6598716feac184426804608e473dbd8
NikolaiSobi <137815949+NikolaiSobi@users.noreply.github.com>
2025-03-17 04:46:38 -0600

    Update db.js

--- FABserver/db.js
+++ FABserver/db.js
@@ -1,9 +1,9 @@
 import pkg from 'pg';
 
 var config = {
-    user: 'nikolai', 
+    user: 'username', // username here for database
     database: 'fleshandblood', 
-    password: 'new_password', 
+    password: 'password', // password here for database
     host: 'localhost', 
     port: 5432,
     options: '-c search_path=FleshAndBlood',
